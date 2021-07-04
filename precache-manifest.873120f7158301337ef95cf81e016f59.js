497de264dcc75407946724c956536f5ee7468a94
zul-adn <zbojetx@gmail.com>
2021-07-04 20:41:19 +0700

    Updates

---
+++ precache-manifest.873120f7158301337ef95cf81e016f59.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e16956b84f0a010472f313330f28a604",
+    "url": "/drainase-admin/index.html"
+  },
+  {
+    "revision": "4424bbfb48b7385fb720",
+    "url": "/drainase-admin/static/css/2.6a59e3ea.chunk.css"
+  },
+  {
+    "revision": "9399503ff8e7d1efe4a4",
+    "url": "/drainase-admin/static/css/main.38258a38.chunk.css"
+  },
+  {
+    "revision": "4424bbfb48b7385fb720",
+    "url": "/drainase-admin/static/js/2.a08f25ff.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/drainase-admin/static/js/2.a08f25ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9399503ff8e7d1efe4a4",
+    "url": "/drainase-admin/static/js/main.32714525.chunk.js"
+  },
+  {
+    "revision": "a549b42d21f24110e7e8",
+    "url": "/drainase-admin/static/js/runtime-main.afa6b150.js"
+  },
+  {
+    "revision": "fb4a7896e97335814a51bf353c587678",
+    "url": "/drainase-admin/static/media/graphic2.fb4a7896.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/drainase-admin/static/media/logo.5d5d9eef.svg"
+  }
+]);
